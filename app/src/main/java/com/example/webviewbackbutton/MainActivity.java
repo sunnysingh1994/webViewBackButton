40e8886ff35722caa5c81328dabb73713a1dd26d
sunnysingh1994 <sunnysingh1994.ss@gmail.com>
2020-01-17 14:49:22 +0530

    implemetation

---
+++ app/src/main/java/com/example/webviewbackbutton/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.webviewbackbutton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webview;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webview = findViewById(R.id.webview);
+        webview.setWebViewClient(new WebViewClient());
+        webview.loadUrl("http://www.google.com");
+
+        WebSettings webSettings = webview.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webview.canGoBack()) {
+            webview.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
